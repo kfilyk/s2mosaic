f292496f5f1b7651c5e05c257bee74ebea708feb
kfilyk <kelvinfilyk@gmail.com>
2021-08-02 11:04:13 -0700

    Added recent changes

--- maps/DataRetr.py
+++ maps/DataRetr.py
@@ -5,8 +5,25 @@ import datetime
 
 #from pyspark import SparkContext
 import matplotlib.pyplot as plt
-from sentinelhub import SHConfig
-from sentinelhub import WmsRequest, WcsRequest, MimeType, CRS, BBox, DataCollection
+from sentinelhub import SHConfig, WmsRequest, WcsRequest, MimeType, CRS, BBox, DataCollection, MimeType, SentinelHubRequest, SentinelHubDownloadClient, bbox_to_dimensions, DownloadRequest
+from utils import plot_image
+
+evalscript_rgb_ir = """
+    function setup() {
+        return {
+            input: [{
+                bands: ["B02", "B03", "B04", "B08"]
+            }],
+            output: {
+                bands: 4
+            }
+        };
+    }
+
+    function evaluatePixel(sample) {
+        return [sample.B08, sample.B04, sample.B03, sample.B02];
+    }
+"""
 
 config = SHConfig()
 
@@ -27,6 +44,7 @@ def plot_image(image, factor=1):
 
 
 sample_coords = [46.16, -16.15, 46.51, -15.580]
+#betsiboka_coords_wgs84 = [46.16, -16.15, 46.51, -15.58]
 
 """All requests require bounding box to be given as an instance of sentinelhub.geometry.BBox with corresponding Coordinate Reference System (sentinelhub.geometry.CRS)"""
 
