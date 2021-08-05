0c811a5e286b6c78c5367b8efa836d6e9a3654bc
kfilyk <kelvinfilyk@gmail.com>
2021-08-04 20:58:10 -0700

    adding updates

--- get_maps.py
+++ get_maps.py
@@ -9,27 +9,53 @@ import dateutil.relativedelta
 import matplotlib.pyplot as plt
 from sentinelhub import SHConfig, WmsRequest, WcsRequest, MimeType, CRS, BBox, DataCollection, MimeType, SentinelHubRequest, SentinelHubDownloadClient, bbox_to_dimensions, DownloadRequest
 from PIL import Image
+from scipy.ndimage.filters import gaussian_filter
+
+
 
 evalscript_10m_bands = """
     //VERSION=3
     function setup() {
         return {
             input: [{
-                bands: ["B02", "B03", "B04", "B08", "CLM"],
-                units: "DN"
+                bands: ["B02", "B03", "B04", "B08", "CLD"],
             }],
             output: {
                 bands: 5,
-                sampleType: "INT16"
+                sampleType: "UINT8",
+                mosaicking: Mosaicking.SIMPLE
             },
-            mosaicking: "ORBIT"
         };
     }
 
     function evaluatePixel(sample) {
-        return [sample.B02, sample.B03, sample.B04, sample.B08, sample.CLM];
+        return [sample.B02*255, sample.B03*255, sample.B04*255, sample.B08*255, sample.CLD];
+
     }
+
 """
+
+def get_map_request(time_interval):
+    return SentinelHubRequest(
+        evalscript=evalscript_10m_bands,
+        input_data=[
+            SentinelHubRequest.input_data(
+                data_collection=DataCollection.SENTINEL2_L2A,  # L2A atmospheric corrected data
+                time_interval=time_interval,
+                maxcc = 0.4,
+                 # if below is commented out, then most recent first
+                mosaicking_order='leastCC'
+            )
+        ],
+        responses=[
+            SentinelHubRequest.output_response(
+                'default', MimeType.TIFF)
+        ],
+        bbox=betsiboka_bbox,
+        size=betsiboka_size,
+        config=config
+    )
+
 
 # sign in to sentinelhub
 config = SHConfig()
@@ -46,7 +72,8 @@ resolution = 10
 
 sites = {}
 # coast of hanoi:
-sites['hanoi'] = [106.00, 13.00, 116.00, 22.50]
+# sites['hanoi'] = [106.00, 13.00, 116.00, 22.50]
+sites['accra'] = [-0.40, 5.20, 0.20, 5.60]
 
 for s in sites:
     start_x = sites[s][0]
@@ -74,51 +101,67 @@ for s in sites:
                     betsiboka_bbox, resolution=resolution)
 
                 """All requests require bounding box to be given as an instance of sentinelhub.geometry.BBox with corresponding Coordinate Reference System (sentinelhub.geometry.CRS)"""
+                # get one month of data
                 today = datetime.today()
-                last_month = (
-                    today + dateutil.relativedelta.relativedelta(months=-36))
+                last_month = today + dateutil.relativedelta.relativedelta(months=-2)
+                last_week = today + dateutil.relativedelta.relativedelta(weeks=-1)
+                slots = []
+                while today > last_month:
+                    
+                    today_str = today.strftime('%Y-%m-%d')  # get in string format
+                    last_week_str = last_week.strftime('%Y-%m-%d')
+                    slots.append((last_week_str, today_str))
+                    today = last_week
+                    last_week = today + dateutil.relativedelta.relativedelta(weeks=-1)
+                print(slots) # should have a number of time intervals
 
-                today = today.strftime('%Y-%m-%d')  # get in string format
-                last_month = last_month.strftime('%Y-%m-%d')
-                print(today)
-                print(last_month)
+                list_of_requests = [get_map_request(slot) for slot in slots]
+                list_of_requests = [request.download_list[0] for request in list_of_requests] # now have a list of requests
+                # download data with multiple threads
+                raw_maps = np.array(SentinelHubDownloadClient(config=config).download(list_of_requests, max_threads=5), dtype=np.float64)
+                #raw_maps = np.array(map_requests.get_data(), dtype=np.float64) # get array of all maps - note that SCL cloud cover map
+                print(raw_maps.shape)  # should be like (3, 13259,1399, 5)
+                print(raw_maps.dtype)
+                maps = np.copy(raw_maps)
+                average_map = np.zeros_like(maps[0, :, :, :4]) # create a single map instance of size (w, h, 4) instead of 5
+                pixel_count = np.zeros_like(maps[0, :, :]) # counts how many different sub map values have been accumulated for a given pixel location
+                print(average_map.shape)  # should be like (3, 13259,1399, 5)
+                print(pixel_count.shape)  # should be like (3, 13259,1399, 5)
 
-                request_bands = SentinelHubRequest(
-                    evalscript=evalscript_10m_bands,
-                    input_data=[
-                        SentinelHubRequest.input_data(
-                            data_collection=DataCollection.SENTINEL2_L2A,  # L2A atmospheric corrected data
-                            time_interval=(last_month, today),
-                            # mosaicking_order='leastCC'
-                        )
-                    ],
-                    responses=[
-                        SentinelHubRequest.output_response(
-                            'default', MimeType.TIFF)
-                    ],
-                    bbox=betsiboka_bbox,
-                    size=betsiboka_size,
-                    config=config
-                )
+                for m in range(0, len(raw_maps)):
+                    map = raw_maps[m] 
+                    for i in range(0, 5):  # R, G, B, IR all need to be normalized
+                        if i==4:
+                            map[:, :, i] = gaussian_filter(map[:, :, i], sigma=3)
+                            for idx, x in np.ndenumerate(map[:, :, i]):
+                                if map[idx[0], idx[1], i] > 0: # if non-zero, indicates some presence of clouds
+                                    map[idx[0], idx[1], 0] = 0
+                                    map[idx[0], idx[1], 1] = 0
+                                    map[idx[0], idx[1], 2] = 0
+                                    map[idx[0], idx[1], 3] = 0
+                            """
+                            min = np.amin(map[:, :, i]) # i==4 has range between 0-100
+                            map[:, :, i] -= min  # center min = 0
+                            max = np.amax(map[:, :, i])
+                            map[:, :, i] *= (255/max) # converts to range 0, 255
+                            """
 
-                # int16 type needs to be scaled by 1/10000. then standardize betweeon 0, 256
-                bands = np.array(request_bands.get_data()[0]) / 10000
-                for i in range(0, 5):
-                    min = np.amin(bands[:, :, i])
-                    max = np.amax(bands[:, :, i])
-                    print("MIN/MAX OG: ")
-                    print(min)
-                    print(max)
-                    bands[:, :, i] = bands[:, :, i]-min  # center at zero
-                    max = np.amax(bands[:, :, i])
-                    bands[:, :, i] *= (255/max)
-                    print(np.amax(bands[:, :, i]))
-                    print(np.amin(bands[:, :, i]))
-                bands = bands.astype(np.uint8)
 
+                    maps[m] = map
+                    map = map.astype(np.uint8) # convert to int8
+                    img_rgb = Image.fromarray(map[:, :, [2, 1, 0]], 'RGB')
+                    img_rgb.show()
+                    #img_ir = Image.fromarray(map[:, :, 3], 'L')
+                    #img_ir.show()
+                    #img_scl = Image.fromarray(map[:, :, 4], 'L')
+                    #img_scl.show()
+                maps = maps.astype(np.uint8) # multiple days of maps
+
+
+                """
                 # generate folder
                 Path(folder_path).mkdir(parents=True, exist_ok=True)
-                for i in range(0, 5):
+                for i in range(0, 4):
                     col = ''
                     if i == 0:
                         col = 'B'
@@ -128,8 +171,6 @@ for s in sites:
                         col = 'R'
                     elif i == 3:
                         col = 'IR'
-                    else:
-                        col = 'CLM'
 
                     data_path = folder_path + '/'+col + \
                         '.png'  # 106.0_13.0_1.jpg
@@ -137,9 +178,10 @@ for s in sites:
                     im = Image.fromarray(bands[:, :, i])
                     im.save(data_path)
 
-                img = Image.fromarray(bands[:, :, [2, 1, 0]], 'RGB')
+                # img = Image.fromarray(bands[:, :, [2, 1, 0]], 'RGB')
                 # img.show()
                 img.save(folder_path+'/col.png')
+                """
             else:
                 print("PATH EXISTS ALREADY...")
             y = round(y+y_delta, 1)
