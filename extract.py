fbe393f9b1d124bfd7ef27888b9349f0dbfe7364
raghav khurana <raghavkhurana@raghavs-MacBook-Pro.local>
2021-08-12 12:31:07 -0700

    extract.py

--- extract.py
+++ extract.py
@@ -5,6 +5,6 @@ for file in os.listdir():
     print(file)
     if(file.endswith(".zip")):
         with zipfile.ZipFile(file, 'r') as zip_ref:
-	    zip_ref.extractall('.')
+            zip_ref.extractall('.')
         os.remove(file)
 
