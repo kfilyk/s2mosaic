ccfa8329a531f93a4d937c010c74ed4d825221a5
Leo Huang <Leo.L.Huang@hotmail.com>
2021-08-14 20:42:56 -0700

    Converted number of days per tile to download into for loop.

--- copernicus.py
+++ copernicus.py
@@ -1,14 +1,18 @@
+# It seems that if you pick a date that is far too back the request gets denied,
+# certain requests also produce 500 internal server error on their server side.
+
 # from sentinelsat import SentinelAPI, read_geojson, geojson_to_wkt
 import sentinelsat
 import glob
 import os
 from datetime import date
 import zipfile
+import operator
 
 # connect to the API
 api = sentinelsat.SentinelAPI('kfilyk', 'wet_garbage')
 
-tiles = ['30NVL', '30NUL', '30NTL', '10UDV', '10UEV', '10UDU', '10UEU', '51RUH', '51RTH', '51RUJ','51RTJ', '48QXH', '48QYJ']
+tiles = ['10UDV']
 #  ghana : 30VNL, 30NUL, 30NTL
 # vancouver/victoria 10UDV, 10UEV, 10UDU, 10UEU
 # taiwan: 51RUH, 51RTH, 51RUJ, 51RTJ
@@ -17,98 +21,96 @@ tiles = ['30NVL', '30NUL', '30NTL', '10UDV', '10UEV', '10UDU', '10UEU', '51RUH',
 query_kwargs = {
     'platformname': 'Sentinel-2',
     'producttype': 'S2MSI2A',  # sentinel 2 multispectrum instrument level 2a
-    'date': ('NOW-30DAYS', 'NOW')}  # < - this scans 30 days into the past
+    'date': ('20210801', '20210813')}  # < - this scans 30 days into the past
 
-# gets the single least cloud covered day from the past 30 days
+# For each area we we need to request grab N number of least cloudy days for it
 for tile in tiles:
     print("TILE: ", tile)
     kw = query_kwargs.copy()
     kw['filename'] = f'*_T{tile}_*'
-    pp = api.query(**kw)
-    print("LEN PP: ", len(pp))
-    print("TYPE PP: ", type(pp))
+    pp = api.query(**kw) # Ordered dictionary
+    # print("LEN PP: ", len(pp))
+    # print("TYPE PP: ", type(pp))
 
-    # collect three days of least cloud cover
-    least_clouds_1 = 100  # this is percentage
-    least_clouds_2 = 100
-    least_clouds_3 = 100
-    least_clouds_1_id = ''
-    least_clouds_2_id = ''
-    least_clouds_3_id = ''
-    for p in pp:  # for map in query results
-        # if tile has even less cloud cover than the current #1, shuffle #1 to #2
-        cc = pp[p]['cloudcoverpercentage']
+    tilesToDownload = {} # Dictionary of (tileId, cloud cover percentage)
+    numTilesToSave = 3
 
-        if cc < least_clouds_1:
-            least_clouds_3_id = least_clouds_2_id
-            least_clouds_3 = least_clouds_2
-            least_clouds_2_id = least_clouds_1_id
-            least_clouds_2 = least_clouds_1
-            least_clouds_1_id = p
-            least_clouds_1 = cc
-        elif cc < least_clouds_2:
-            least_clouds_3_id = least_clouds_2_id
-            least_clouds_3 = least_clouds_2
-            least_clouds_2_id = p
-            least_clouds_2 = cc
-        elif cc < least_clouds_3:
-            least_clouds_3_id = p
-            least_clouds_3 = cc
+    for p in pp:
+        # print(pp[p]['cloudcoverpercentage'])
+        if len(tilesToDownload) < numTilesToSave:
+            tilesToDownload.update({p: pp[p]['cloudcoverpercentage']})
+        else:
+            # Replace if current cloud coverage is lower than highest in the list
+            currentMaxCCId = max(tilesToDownload, key=tilesToDownload.get)
+            if pp[p]['cloudcoverpercentage'] < tilesToDownload[currentMaxCCId]:
+                # print (pp[p]['cloudcoverpercentage'], " smaller than ", tilesToDownload[currentMaxCCId])
+                tilesToDownload.pop(currentMaxCCId)
+                tilesToDownload.update({p: pp[p]['cloudcoverpercentage']})
 
-    print("#1. %s: %d" % (least_clouds_1_id, least_clouds_1))
-    print("#2. %s: %d" % (least_clouds_2_id, least_clouds_2))
-    print("#3. %s: %d" % (least_clouds_3_id, least_clouds_3))
+    tilesSortedByCC = sorted(tilesToDownload.items(), key=operator.itemgetter(1)) #  Sort dictionary from lowest to highest
+    print("Tile (", tile,")'s least cloud tiles: ", tilesSortedByCC)
+
+    for sortedTile in tilesSortedByCC:
+        if not os.path.exists("./maps/" + tile + "/"+pp[sortedTile[0]]['title'] + ".SAFE"):
+            print(pp[sortedTile[0]]['title'] + " doesn't exist yet. Downloading...")
+            try:
+                api.download(sortedTile[0], "./maps/"+tile)
+            except:
+                pass
+        else:
+            print(pp[sortedTile[0]]['title'] + " already exists.")
 
     # extract all previously downloaded and unzipped
-    for file in os.listdir('./maps/'+tile):
-        print(file)
-        if(file.endswith(".zip")):
-            print("UNZIPPING %s..." %(file))
-            with zipfile.ZipFile('./maps/'+tile+'/'+file, 'r') as zip_ref:
-                zip_ref.extractall('./maps/'+tile)
-            #os.remove(file)
+    # for file in os.listdir('./maps/'+tile):
+    #     print(file)
+    #     if(file.endswith(".zip")):
+    #         print("UNZIPPING %s..." %(file))
+    #         with zipfile.ZipFile('./maps/'+tile+'/'+file, 'r') as zip_ref:
+    #             zip_ref.extractall('./maps/'+tile)
+    #         #os.remove(file)
 
-    # we have top 3 cloudless days for each tile in 'tiles' list: download to its own folder
-    if not os.path.exists('./maps/'+tile+"/"+pp[least_clouds_1_id]['title']+'.SAFE'):
-        print(pp[least_clouds_1_id]['title'] +
-              " doesn't exist yet. Downloading...")
-        try:
-            # sensat automatically skips downloading previously downloaded files??
-            api.download(least_clouds_1_id, './maps/'+tile)
-        except:
-            # if connection gets interrupted
-            pass
-    else:
-        print(pp[least_clouds_1_id]['title']+" already exists.")
+    # # we have top 3 cloudless days for each tile in 'tiles' list: download to its own folder
+    # if not os.path.exists('./maps/'+tile+"/"+pp[least_clouds_1_id]['title']+'.SAFE'):
+    #     print(pp[least_clouds_1_id]['title'] +
+    #           " doesn't exist yet. Downloading...")
+    #     try:
+    #         # sensat automatically skips downloading previously downloaded files??
+    #         api.download(least_clouds_1_id, './maps/'+tile)
+    #     except:
+    #         # if connection gets interrupted
+    #         pass
+    # else:
+    #     print(pp[least_clouds_1_id]['title']+" already exists.")
 
-    if not os.path.exists('./maps/'+tile+"/"+pp[least_clouds_2_id]['title']+'.SAFE'):
-        print(pp[least_clouds_2_id]['title'] +
-              " doesn't exist yet. Downloading...")
-        try:
-            api.download(least_clouds_2_id, './maps/'+tile)
-        except:
-            # if connection gets interrupted
-            pass
-    else:
-        print(pp[least_clouds_2_id]['title']+" already exists.")
+    # if not os.path.exists('./maps/'+tile+"/"+pp[least_clouds_2_id]['title']+'.SAFE'):
+    #     print(pp[least_clouds_2_id]['title'] +
+    #           " doesn't exist yet. Downloading...")
+    #     try:
+    #         api.download(least_clouds_2_id, './maps/'+tile)
+    #     except:
+    #         # if connection gets interrupted
+    #         pass
+    # else:
+    #     print(pp[least_clouds_2_id]['title']+" already exists.")
 
-    print('./maps/'+tile+"/"+pp[least_clouds_3_id]['title']+'.SAFE')
-    if not os.path.exists('./maps/'+tile+"/"+pp[least_clouds_3_id]['title']+'.SAFE'):
-        print(pp[least_clouds_3_id]['title'] +
-              " doesn't exist yet. Downloading...")
-        try:
-            api.download(least_clouds_3_id, './maps/'+tile)
-        except:
-            pass
-    else:
-        print(pp[least_clouds_3_id]['title']+" already exists.")
+    # print('./maps/'+tile+"/"+pp[least_clouds_3_id]['title']+'.SAFE')
+    # if not os.path.exists('./maps/'+tile+"/"+pp[least_clouds_3_id]['title']+'.SAFE'):
+    #     print(pp[least_clouds_3_id]['title'] +
+    #           " doesn't exist yet. Downloading...")
+    #     try:
+    #         api.download(least_clouds_3_id, './maps/'+tile)
+    #     except:
+    #         pass
+    # else:
+    #     print(pp[least_clouds_3_id]['title']+" already exists.")
 
-    for file in os.listdir('./maps/'+tile):
-        print(file)
-        if(file.endswith(".zip")):
-            print("UNZIPPING %s..." %(file))
-            with zipfile.ZipFile('./maps/'+tile+'/'+file, 'r') as zip_ref:
-                zip_ref.extractall('./maps/'+tile)
+
+    # for file in os.listdir('./maps/'+tile):
+    #     print(file)
+    #     if(file.endswith(".zip")):
+    #         print("UNZIPPING %s..." %(file))
+    #         with zipfile.ZipFile('./maps/'+tile+'/'+file, 'r') as zip_ref:
+    #             zip_ref.extractall('./maps/'+tile)
             #os.remove(file)
 
 """
