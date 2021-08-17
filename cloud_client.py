0ef319a86a76c0f3766ef7388153f56a4e9a4892
raghav khurana <raghavkhurana@raghavs-MacBook-Pro.local>
2021-08-16 19:37:04 -0700

    cloud_client.py

---
+++ cloud_client.py
@@ -0,0 +1,17 @@
+from img_proc import BUFFER_SIZE, SEPARATOR
+import os
+import socket
+import tqdm
+
+SEPARATOR = "<SEPARATOR>"
+BUFFER_SIZE = 4096
+
+host = "192.168.171.36"
+
+port = 5001
+
+s = socket.socket()
+print(f"[+] Connecting to {host}:{port}")
+
+s.connect((host, port))
+print("[+] Connected.")
