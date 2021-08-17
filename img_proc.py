cd54b2fb19997fca8586a608bdd1e2bc80acf357
raghav khurana <raghavkhurana@raghavs-MacBook-Pro.local>
2021-08-17 02:33:38 -0700

    img_proc.py

--- img_proc.py
+++ img_proc.py
@@ -16,23 +16,28 @@ SERVER_HOST = "34.133.186.193"
 s = socket.socket()
 path = "/home/ubuntu/wet_garbage/maps/"
 
-for files in os.listdir(path):
-	for data in os.walk(path+files):
-		if(data.__contains__(".jp2")):
-   			 filelist.append(data)
-print(filelist)
 
+for dirpath, dirnames, filenames in os.walk("."):
+    for filename in [f for f in filenames if f.endswith(".jp2")]:
+        filelist.append(os.path.join(dirpath,filename))
+
+NTL = [:3]
 
 for name in filelist:
-	with rasterio.open(jp2) as f:
-         jp_data.append(f.read(1))
-         np_data = np.array(jp_data, dtype=jp_data[0].dtype)
-         print(np_data)
+    if "NTL" in name:
+        if "10m" in name:
+            NTL[0].append(name)
+        if "20m" in name:
+            NTL[1].append(name)
+        if "60m" in name:
+            NTL[2].append(name)
 
 
+data = np.array(jp_data, dtype=jp_data[0].dtype)
+print(data.shape)
 s.connect((SERVER_HOST, SERVER_PORT))
 print("[+] Connected.")
-
-
-	
+filename = "np_array"
+filesize = os.path.getsize(filename)
+s.send(f"{filename}{SEPARATOR}{filesize}".encode())
 
