5f6c32582f95fc97cf02d4e264590d06e79844f8
kfilyk <kelvinfilyk@gmail.com>
2021-08-18 15:21:41 -0700

    Adding scp_proc

--- scp_subproc.py
+++ scp_subproc.py
@@ -1,21 +1,16 @@
 import subprocess
 import os
 import shlex
-
-
 path = "./maps/"
 files = []
 filelist = []
-
 for dirpath, dirnames, filenames in os.walk("."):
-    for filename in [f for f in filenames if f.endswith(".png")]:
-        filelist.append(os.path.join(dirpath,filename))
-        files.append(filename)
-
+  for filename in [f for f in filenames if f.endswith(".png")]:
+    filelist.append(os.path.join(dirpath,filename))
+    files.append(filename)
 print(files)
 print(filelist)
-
 for x in filelist:
-    project_list_command = ("scp -i ~/home/ubuntu/.ssh %s raghavkhurana@34.133.186.193:/~ " % x)
-    project_output = subprocess.check_output(shlex.split(project_list_command))
-    print(project_output)
+  project_list_command = ("gcloud compute scp %s raghavkhurana@instance-1:~ --zone us-central1-a" % x)
+  project_output = subprocess.check_output(project_list_command,shell = True)
+  print(project_output)
