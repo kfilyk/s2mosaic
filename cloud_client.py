409407c16a1ebc3838b53091503819ff89c15c40
raghav khurana <raghavkhurana@raghavs-MacBook-Pro.local>
2021-08-17 20:44:03 -0700

    cloud_client.py

--- cloud_client.py
+++ cloud_client.py
@@ -1,15 +1,10 @@
-<<<<<<< HEAD
 
-=======
->>>>>>> fa4c1749a9f4dfb8283eb5f46fcd64fcdee47762
 import os
 import socket
 import tqdm
 
 SEPARATOR = "<SEPARATOR>"
-<<<<<<< HEAD
-BUFFER_SIZE = 1024
-=======
+
 BUFFER_SIZE = 4096
 >>>>>>> fa4c1749a9f4dfb8283eb5f46fcd64fcdee47762
 
@@ -24,7 +19,7 @@ s.listen(5)
 print(f"[*] Listening as{host}:{port}")
 client_socket, address = s.accept()
 print(f"[+] {address} is connected")
-<<<<<<< HEAD
+
 while 1:
     received = client_socket.recv(BUFFER_SIZE).decode()
     print(received)
@@ -43,12 +38,3 @@ while 1:
 
 client_socket.close()
 s.close()
-=======
-received = client_socket.recv(BUFFER_SIZE).decode()
-filename, filesize = received.split(SEPARATOR)
-# remove absolute path if there is
-filename = os.path.basename(filename)
-# convert to integer
-filesize = int(filesize)
-print(filename,filesize)
->>>>>>> fa4c1749a9f4dfb8283eb5f46fcd64fcdee47762
